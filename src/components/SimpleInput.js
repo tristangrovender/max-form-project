19c03ed4e6b19b9af67e023292ded9e56d82aee0
Tristan Grovender <46428421+tristangrovender@users.noreply.github.com>
2021-11-23 16:16:30 -0600

    Adding basic validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -10,6 +10,10 @@ const SimpleInput = props => {
   const formSubmissionHandler = event => {
     event.preventDefault();
 
+    if (enteredName.trim() === "") {
+      return;
+    }
+
     console.log(enteredName);
     setEnteredName("");
   };
