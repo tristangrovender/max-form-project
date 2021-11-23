4dc2b064e6a23b9e1e237737c2b3d37471192ccc
Tristan Grovender <46428421+tristangrovender@users.noreply.github.com>
2021-11-23 16:35:09 -0600

    react to lost focus on input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -9,6 +9,15 @@ const SimpleInput = props => {
     setEnteredName(event.target.value);
   };
 
+  const nameInputBlurHandler = event => {
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+  };
+
   const formSubmissionHandler = event => {
     event.preventDefault();
 
@@ -40,6 +49,7 @@ const SimpleInput = props => {
           id="name"
           onChange={nameInputChangeHandler}
           value={enteredName}
+          onBlur={nameInputBlurHandler}
         />
         {nameInputIsInvalid && (
           <p className="error-text">Name must not be empty.</p>
